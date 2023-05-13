d714446dbc8fa6de0f058d9786372ec31b70d690
Mata13 <marc.dev121@gmail.com>
2023-05-13 17:42:51 -0600

    Some basic methods in strings

--- 03-Some-Methods/Program.cs
+++ 03-Some-Methods/Program.cs
@@ -17,3 +17,18 @@ Console.WriteLine("............................");
 
 Console.WriteLine("Substrings");
 Console.WriteLine("............................");
+
+string str2 = str.Substring(1, 4); // este metodo nos devuelve un substring (i inicial, length)
+Console.WriteLine(str2);
+
+string str3 = str.Substring(0, 5); // este metodo nos devuelve un substring (i inicial, length)
+Console.WriteLine(str3);
+
+string str4 = str.Substring(3, 8); // este metodo nos devuelve un substring (i inicial, length)
+Console.WriteLine(str4);
+
+string str5 = str.Substring(0, 4); // este metodo nos devuelve un substring (i inicial, length)
+Console.WriteLine(str5);
+
+string str6 = str.Substring(6, 3); // este metodo nos devuelve un substring (i inicial, length)
+Console.WriteLine(str6);
