bdfcbe8b4994c51f8f6d0e5d42d916960962df7f
Mata13 <marc.dev121@gmail.com>
2023-05-13 15:09:35 -0600

    Some basic methods

---
+++ 03-Some-Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿// Some basic methods in C#
+
+// STRINGS
+
+Console.WriteLine("............................");
+Console.WriteLine("STRINGS");
+Console.WriteLine("............................");
+
+Console.WriteLine("Length");
+Console.WriteLine("............................");
+
+string str = "Hello, World!"; // definimos la variable
+int length = str.Length; // metodo lenght determina la cantidad de caracteres del string
+Console.WriteLine(length);
+
+Console.WriteLine("............................");
+
+Console.WriteLine("Substrings");
+Console.WriteLine("............................");
