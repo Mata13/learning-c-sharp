99c09a0ff84a7018c12fdfc94388fbe698ce9e24
Mata13 <marc.dev121@gmail.com>
2023-07-14 21:47:21 -0600

    cambios simples

--- 07-functions/02-funcParimpar/Program.cs
+++ 07-functions/02-funcParimpar/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿// Funcion par-impar
+
+/*
+
+Console.WriteLine("Ingresa un numero: "); // ingresamos un numero por consola
+;
+int num = int.Parse(Console.ReadLine()); // convertimos el string a entero
+
+// Console.WriteLine($"El numero que ingresaste es: {num}"); // comprobamos el numero ingresado
+
+// comprobamos si el numero es par o impar
+
+if (num % 2 == 0) {
+	Console.WriteLine($"El numero {num} es par.");
+} else
+{
+	Console.WriteLine($"El numero {num} es impar");
+}
+
+*/
+
+using System;
+namespace MyProgram
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Ingresa un numero entero positivo:");
+		}
+
+		static void CheckEvenOdd(int num)
+		{
+			if (num % 2 == 0)
+			{
+				Console.WriteLine("El numero {num} es par");
+			}
+			else
+			{
+				Console.WriteLine("El numero {num} es impar");
+			}
+		}
+
+	}
+}
---
+++ Projects/03-carritoCompras/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main()
+	{
+		List<string> carrito = new List<string>(); // definimos la lista vacia
+
+		bool compra = true;
+
+		while (compra)
+		{
+			Console.WriteLine("...................");
+			Console.WriteLine("Bienvenid@ al CHIKERO PRO");
+			Console.WriteLine("...................");
+
+			Console.WriteLine("Presiona enter para continuar: ");
+			Console.ReadKey();
+
+			Console.WriteLine("...................");
+			Console.WriteLine("Menu de opciones");
+
+			Console.WriteLine("1. Agregar un producto al carrito");
+			Console.WriteLine("2. Ver carrito");
+			Console.WriteLine("3. Pagar");
+			Console.WriteLine("4. Cancelar");
+			Console.WriteLine("...................");
+
+			Console.WriteLine("Elige una opcion para continuar");
+			string opcionUsuario = Console.ReadLine();
+
+			// Console.WriteLine(opcionUsuario);
+
+			// break;
+
+			switch (opcionUsuario)
+			{
+				case "1":
+					Console.WriteLine("Ingresa el nombre del producto");
+					string producto = Console.ReadLine();
+					carrito.Add(producto);
+					Console.WriteLine($"'{producto}' agregado al carrito.");
+					break;
+
+				case "2":
+					Console.WriteLine("Ver el carrito");
+					foreach (string elementoCarrito in carrito)
+					{
+						Console.WriteLine(elementoCarrito);
+					}
+					break;
+				case "3":
+					Console.WriteLine("Pagando...");
+					Console.WriteLine("Gracias por tu compra!!!");
+					compra = false;
+					break;
+				case "4":
+					Console.WriteLine("Cancelando...");
+					compra = false;
+					break;
+				default:
+				Console.WriteLine("Opcion incorrecta. Intenta de nuevo");
+				break;
+			}
+
+		}
+	}
+}
